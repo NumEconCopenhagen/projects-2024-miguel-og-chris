750130f802715e0906ed3213a55da6fb0803196a
ChristopherAsboe <bvj626@alumni.ku.dk>
2024-03-14 18:10:01 +0100

    Solve edgeworth

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -32,7 +32,9 @@ class ExchangeEconomyClass:
 
         # b. endowments
         par.w1A = 0.8
+        par.w1B = 0.2
         par.w2A = 0.3
+        par.w2B = 0.7
 
     def utility_A(self,x1A,x2A):
         return x1A**self.par.alpha * x2A**(1-self.par.alpha)
@@ -59,47 +61,3 @@ class ExchangeEconomyClass:
 
         return eps1,eps2
     
-
-# Edgeworth boks
-def draw_edgeworth:
-    model = ExchangeEconomyClass()
-    par = model.par
-
-# a. total endowment
-    w1bar = 1.0
-    w2bar = 1.0
-
-# b. figure set up
-    fig = plt.figure(frameon=False,figsize=(6,6), dpi=100)
-    ax_A = fig.add_subplot(1, 1, 1)
-
-    ax_A.set_xlabel("$x_1^A$")
-    ax_A.set_ylabel("$x_2^A$")
-
-    temp = ax_A.twinx()
-    temp.set_ylabel("$x_2^B$")
-    ax_B = temp.twiny()
-    ax_B.set_xlabel("$x_1^B$")
-    ax_B.invert_xaxis()
-    ax_B.invert_yaxis()
-
-# A
-    ax_A.scatter(par.w1A,par.w2A,marker='s',color='black',label='endowment')
-
-# limits
-    ax_A.plot([0,w1bar],[0,0],lw=2,color='black')
-    ax_A.plot([0,w1bar],[w2bar,w2bar],lw=2,color='black')
-    ax_A.plot([0,0],[0,w2bar],lw=2,color='black')
-    ax_A.plot([w1bar,w1bar],[0,w2bar],lw=2,color='black')
-
-    ax_A.set_xlim([-0.1, w1bar + 0.1])
-    ax_A.set_ylim([-0.1, w2bar + 0.1])    
-    ax_B.set_xlim([w1bar + 0.1, -0.1])
-    ax_B.set_ylim([w2bar + 0.1, -0.1])
-
-    ax_A.legend(frameon=True,loc='upper right',bbox_to_anchor=(1.6,1.0));
-
-N = 1000
-x1_vec = np.linspace(0,1,N)
-
-for
